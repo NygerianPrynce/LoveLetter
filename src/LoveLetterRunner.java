62b55ecf940090897b957180dbeca01b873b9608
NygerianPrynce <89479939+NygerianPrynce@users.noreply.github.com>
2022-09-12 19:19:05 -0500

    backtonormal

--- src/LoveLetterRunner.java
+++ src/LoveLetterRunner.java
@@ -3,9 +3,6 @@ import java.util.Scanner;
 public class LoveLetterRunner{
     public static void main(String[] args) {
         LoveLetterGraphic graphicWindow = new LoveLetterGraphic("LoveLetter");
-        //change
-        System.out.println("there has been a change");
-
 
 
 
